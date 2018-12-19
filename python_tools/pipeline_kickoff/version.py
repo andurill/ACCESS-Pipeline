23630029328f2dc6db2723463e008f90213075be
andurill <gowtham.jn@gmail.com>
2018-12-19 08:33:28 -0500

    fixed untracked files

--- python_tools/pipeline_kickoff/version.py
+++
@@ -1,5 +0,0 @@
-most_recent_tag = '0.0.33_dmp'
-dirty = True
-version = 'f874bd8a9bb7da9839f0aa6be708d5d16f72945d-dirty'
-short_version = 'f874bd8-dirty'
-current_commit = 'f874bd8a9bb7da9839f0aa6be708d5d16f72945d'
